b9a1174ccd9435b8a9f776e321b7ae7c11354579
Santosh <santosharakere@gmail.com>
2014-02-03 10:07:22 +0000

    sync test.sh

--- Scripts/test.sh
+++ Scripts/test.sh
@@ -1,3 +1,3 @@
 rm *.jtl
 rm *.csv
-/var/lib/jenkins/jmeter/apache-jmeter-2.11/bin/jmeter -n -t myTestAction.jmx
+/var/lib/jenkins/jmeter/apache-jmeter-2.11/bin/jmeter -n -t ./JMeterCI/Scripts/myTestAction.jmx
